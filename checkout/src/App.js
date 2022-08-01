bbbcf23f2e8db334faeedda4c79dc9e9063e03fa
BruceDorsey <bruce.dorsey@cdpipelinedevshop.com>
2022-08-01 16:58:44 +0000

    checkout

--- checkout/src/App.js
+++ checkout/src/App.js
@@ -1,10 +1,13 @@
 import Checkout from './components/Checkout';
 import './App.css';
+import Bannerimage from '/workspace/checkout-form/checkout/src/components/Bannerimage.js';
 
 function App() {
   return (
     <div className="App">
       <Checkout />
+      <Bannerimage/>
+
     </div>
   );
 }
---
+++ checkout/src/components/Bannerimage.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import '/workspace/checkout-form/checkout/src/App.css';
+
+function Bannerimage () {
+
+return (
+    <header>
+    <header id="bannerimage"/>
+</header>
+)
+}
+export default Bannerimage;
