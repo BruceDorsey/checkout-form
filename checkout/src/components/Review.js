308e1f354298b08cec0001b858ebb62271596b29
BruceDorsey <bruce.dorsey@cdpipelinedevshop.com>
2022-08-03 15:04:10 +0000

    checkout

--- checkout/src/components/Review.js
+++ checkout/src/components/Review.js
@@ -7,29 +7,34 @@ import Grid from '@mui/material/Grid';
 
 const products = [
   {
-    name: 'Product 1',
-    desc: 'A nice thing',
+    image: <img src="https://imgs.search.brave.com/oF7rkHtXdIIY-dlici7M3oRuMvvUmxf16jkc2dGBKD8/rs:fit:678:800:1/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL0kv/NzFDZFR1VmlPS0wu/X0FDX1VMODAwX1FM/NjVfLmpwZw" width="180px" height="200px"/>,
+    name: 'FNAF: The Fazbear Cringe',
+    desc: 'Cringe Audiobooks',
     price: '$9.99',
   },
   {
-    name: 'Product 2',
-    desc: 'Another thing',
+    image: <img src="https://imgs.search.brave.com/YAVwFMir6DPol8wR_wjTCul_wFLwn-5JDVN2Z5VNk24/rs:fit:990:1200:1/g:ce/aHR0cDovLzMuYnAu/YmxvZ3Nwb3QuY29t/Ly1ZeUx1SE13b2tn/WS9WUFRCVjB5MkN1/SS9BQUFBQUFBQUhP/SS8yVUdNa0VDMVcz/by9zMTYwMC90aGVn/aXZlci5qcGc" width="180px" height="200px"/>,
+    name: '"The Giver" by Lois Lowry',
+    desc: '10/10 Inspiring Read',
     price: '$3.45',
   },
   {
-    name: 'Product 3',
-    desc: 'Something else',
+    image: <img src="https://imgs.search.brave.com/qNqiA1s9eeEhbVDTBXu3XR2EcLQFYXofhmQDIc-GFfE/rs:fit:1200:1200:1/g:ce/aHR0cDovL2ltYWdl/czIuZmFucG9wLmNv/bS9pbWFnZXMvcGhv/dG9zLzc2MDAwMDAv/Q29yYWxpbmUtYm9v/ay1jb3JhbGluZS03/NjQ1Nzk3LTE4MTUt/MjU2MC5qcGc" width="180px" height="200px"/>,
+    name: '"Coraline" by Neil Gaimen',
+    desc: 'A Spooky Read',
     price: '$6.51',
   },
   {
-    name: 'Product 4',
-    desc: 'Best thing of all',
+    image: <img src="https://imgs.search.brave.com/f-7_N2zLXiS-lt63Nekr-QAz74fBwbfo1fKFZPPPThI/rs:fit:317:475:1/g:ce/aHR0cHM6Ly9pLmdy/LWFzc2V0cy5jb20v/aW1hZ2VzL1MvY29t/cHJlc3NlZC5waG90/by5nb29kcmVhZHMu/Y29tL2Jvb2tzLzE1/ODkxNDY1MDFsLzUz/MzYzOTA2Ll9TWTQ3/NV8uanBn" width="180px" height="200px"/>,
+    name: 'REDACTED',
+    desc: 'Not  Authorized to Read',
     price: '$14.11',
   },
   { name: 'Shipping', desc: '', price: 'Free' },
 ];
 
-const addresses = ['1 MUI Drive', 'Reactville', 'Anytown', '99999', 'USA'];
+const addresses =[
+  '9824 Hill Field St. Charlottesville, VA 22901'];
 const payments = [
   { name: 'Card type', detail: 'Visa' },
   { name: 'Card holder', detail: 'Mr Dorsey' },
@@ -46,6 +51,7 @@ export default function Review() {
       <List disablePadding>
         {products.map((product) => (
           <ListItem key={product.name} sx={{ py: 1, px: 0 }}>
+            {product.image}
             <ListItemText primary={product.name} secondary={product.desc} />
             <Typography variant="body2">{product.price}</Typography>
           </ListItem>
